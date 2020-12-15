6fd495d1f47c82dbc2d08d5aecffe260e5cc7025
Pedro Moreno <nennogabriel@gmail.com>
2020-12-14 23:47:51 -0300

    -mform test

--- src/components/form/NewsletterForm/FormComponent.tsx
+++ src/components/form/NewsletterForm/FormComponent.tsx
@@ -46,7 +46,6 @@ export default function App() {
       })
         .then(() => {
           navigate("/newsletter/obrigado")
-          // alert('Mensagem enviada, Obrigado por se registrar');
           resetForm()
         })
         .catch(error => {
